77ad87429d0bb0a79da9bb0dadd096c9e52171ef
David J. Gardner <gardner48@llnl.gov>
2021-05-18 14:15:19 -0700

    add different versions of G function

--- examples/stripped_down_tango_example_pq_kinsol.py
+++ examples/stripped_down_tango_example_pq_kinsol.py
@@ -83,7 +83,7 @@ class Problem:
 
         # problem setup
         L = 1                             # size of domain
-        Problem.N = 500                   # number of spatial grid points
+        Problem.N = args.N                # number of spatial grid points
         Problem.dx = L / (Problem.N - 1)  # spatial grid size
 
         # location corresponding to grid points j=0, ..., N-1
@@ -134,8 +134,87 @@ class Problem:
 
         Problem.iter_idx = 0
 
+        Problem.flux = np.zeros_like(Problem.x)
 
-    def Gfun(sunvec_old, sunvec_new, user_data):
+
+    def Gfun_p(sunvec_old, sunvec_new, user_data):
+
+        try:
+            # extract old profile and flux
+            state_old = kin.N_VGetData(sunvec_old)
+            profile = state_old[:Problem.N]
+
+            # get turbulent flux
+            flux = Problem.fluxModel.get_flux(profile)
+
+            # relax flux
+            if Problem.iter_idx == 0:
+                Problem.flux = flux
+            else:
+                Problem.flux = (1 - Problem.args.beta) * Problem.flux + Problem.args.beta * flux
+
+            # transform flux into effective transport coefficients.  H2=D, H3=-c
+            # [use flux split class from lodestro_method]
+            (D, c, _) = Problem.fluxSplitter.flux_to_transport_coeffs(Problem.flux,
+                                                                      profile,
+                                                                      Problem.dx)
+
+            H2Turb = D
+            H3 = -c
+
+            # get H's for all the others (H1, H2, H7).  H's represent terms in the transport equation
+            H1 = np.ones_like(Problem.x)
+            H7 = source(Problem.x)
+            H2const = 0.00  # could represent some background level of (classical) diffusion
+            H2 = H2Turb + H2const
+
+            ## new --- discretize, then compute the residual, then solve the matrix equation for the new profile
+            (A, B, C, f) = HToMatrixFD.H_to_matrix(Problem.dt,
+                                                   Problem.dx,
+                                                   Problem.nL,
+                                                   Problem.n_IC,
+                                                   H1, H2=H2, H3=H3, H7=H7)
+
+            # see fieldgroups.calculate_residual() for additional information on the residual calculation
+            resid = A*np.concatenate((profile[1:], np.zeros(1))) + B*profile + C*np.concatenate((np.zeros(1), profile[:-1])) - f
+            resid = resid / np.max(np.abs(f))  # normalize the residual
+            rmsResid = np.sqrt( np.mean( resid**2 ))  # take an rms characterization of residual
+            Problem.residual_history[Problem.iter_idx] = rmsResid
+
+            # error
+            Problem.error_history[Problem.iter_idx] = np.max(np.abs(profile - Problem.profile_ss))
+
+            # save data
+            if Problem.args.makeplots:
+                Problem.profile_history[Problem.iter_idx, :] = profile
+                Problem.flux_history[Problem.iter_idx, :] = Problem.flux
+
+            # solve matrix equation for new profile
+            profile_new = HToMatrixFD.solve(A, B, C, f)
+
+            # relax profile
+            profile_new = (1 - Problem.args.alpha) * profile + Problem.args.alpha * profile_new
+
+            # check
+            if np.any(profile_new < 0) == True:
+                print(f'error.  negative value detected in profile at iter = {Problem.iter_idx}')
+                raise ValueError
+
+            # extract old profile and flux
+            state_new = kin.N_VGetData(sunvec_new)
+            state_new[:Problem.N] = profile_new
+
+            # update iteration count
+            Problem.iter_idx += 1
+
+        except:
+            print(f'error.  Gfun_p failed at iter = {Problem.iter_idx}')
+            return -1
+        else:
+            return 0
+
+
+    def Gfun_pq(sunvec_old, sunvec_new, user_data):
 
         try:
             # extract old profile and flux
@@ -205,7 +284,86 @@ class Problem:
             Problem.iter_idx += 1
 
         except:
-            print(f'error.  Gfun failed at iter = {Problem.iter_idx}')
+            print(f'error.  Gfun_pq failed at iter = {Problem.iter_idx}')
+            return -1
+        else:
+            return 0
+
+
+    def Gfun_d(sunvec_old, sunvec_new, user_data):
+
+        try:
+            # extract old profile and flux
+            state_old = kin.N_VGetData(sunvec_old)
+            profile = state_old[:Problem.N]
+
+            # get turbulent flux
+            flux = Problem.fluxModel.get_flux(profile)
+
+            # transform flux into effective transport coefficients.  H2=D, H3=-c
+            # [use flux split class from lodestro_method]
+            (D, c, _) = Problem.fluxSplitter.flux_to_transport_coeffs(flux,
+                                                                      profile,
+                                                                      Problem.dx)
+
+            # compute relaxation of D, c
+            # (EWMA = Exponentially Weighted Moving Average)
+            if Problem.iter_idx == 0:
+                Problem.D_EWMA = D
+                Problem.c_EWMA = c
+            else:
+                Problem.D_EWMA = (Problem.args.gamma * D +
+                                  (1 - Problem.args.gamma) * Problem.D_EWMA)
+                Problem.c_EWMA = (Problem.args.gamma * c +
+                                  (1 - Problem.args.gamma) * Problem.c_EWMA)
+
+            H2Turb = Problem.D_EWMA
+            H3 = -Problem.c_EWMA
+
+            # get H's for all the others (H1, H2, H7).  H's represent terms in the transport equation
+            H1 = np.ones_like(Problem.x)
+            H7 = source(Problem.x)
+            H2const = 0.00  # could represent some background level of (classical) diffusion
+            H2 = H2Turb + H2const
+
+            ## new --- discretize, then compute the residual, then solve the matrix equation for the new profile
+            (A, B, C, f) = HToMatrixFD.H_to_matrix(Problem.dt,
+                                                   Problem.dx,
+                                                   Problem.nL,
+                                                   Problem.n_IC,
+                                                   H1, H2=H2, H3=H3, H7=H7)
+
+            # see fieldgroups.calculate_residual() for additional information on the residual calculation
+            resid = A*np.concatenate((profile[1:], np.zeros(1))) + B*profile + C*np.concatenate((np.zeros(1), profile[:-1])) - f
+            resid = resid / np.max(np.abs(f))  # normalize the residual
+            rmsResid = np.sqrt( np.mean( resid**2 ))  # take an rms characterization of residual
+            Problem.residual_history[Problem.iter_idx] = rmsResid
+
+            # error
+            Problem.error_history[Problem.iter_idx] = np.max(np.abs(profile - Problem.profile_ss))
+
+            # save data
+            if Problem.args.makeplots:
+                Problem.profile_history[Problem.iter_idx, :] = profile
+                Problem.flux_history[Problem.iter_idx, :] = flux
+
+            # solve matrix equation for new profile
+            profile_new = HToMatrixFD.solve(A, B, C, f)
+
+            # check
+            if np.any(profile_new < 0) == True:
+                print(f'error.  negative value detected in profile at iter = {Problem.iter_idx}')
+                raise ValueError
+
+            # extract old profile and flux
+            state_new = kin.N_VGetData(sunvec_new)
+            state_new[:Problem.N] = profile_new
+
+            # update iteration count
+            Problem.iter_idx += 1
+
+        except:
+            print(f'error.  Gfun_p failed at iter = {Problem.iter_idx}')
             return -1
         else:
             return 0
@@ -230,7 +388,12 @@ class Problem:
                 raise RuntimeError(f'KINSetMAA returned {flag}')
 
         # wrap the python system function so that it is callable from C
-        sysfn = kin.WrapPythonSysFn(Problem.Gfun)
+        if Problem.args.gfun == 'pq':
+            sysfn = kin.WrapPythonSysFn(Problem.Gfun_pq)
+        elif Problem.args.gfun == 'p':
+            sysfn = kin.WrapPythonSysFn(Problem.Gfun_p)
+        else:
+            sysfn = kin.WrapPythonSysFn(Problem.Gfun_d)
 
         # initialize KINSOL
         flag = kin.KINInitPy(kmem, sysfn, sunvec_state)
@@ -313,6 +476,13 @@ class Problem:
 
 parser = argparse.ArgumentParser(description='Run Shestakov example')
 
+
+parser.add_argument('--N', type=int, default=500,
+                    help='Number of spatial mesh points')
+
+parser.add_argument('--gfun', default='p', choices=['pq', 'p', 'd'],
+                    help='Fixed-point function formulation')
+
 parser.add_argument('--p', type=int, default=2,
                     help='Power for analytic flux')
 
@@ -331,6 +501,9 @@ parser.add_argument('--alpha', type=float, default=1.0,
 parser.add_argument('--beta', type=float, default=1.0,
                     help='Relaxation parameter for flux')
 
+parser.add_argument('--gamma', type=float, default=1.0,
+                    help='Relaxation parameter for transport coefficients')
+
 parser.add_argument('--max_iterations', type=int, default=200,
                     help='Maximum number iterations')
 
@@ -340,7 +513,7 @@ parser.add_argument('--aa_m', type=int, default=0,
 parser.add_argument('--aa_delay', type=int, default=0,
                     help='Anderson acceleration delay')
 
-parser.add_argument('--aa_damping', type=int, default=1.0,
+parser.add_argument('--aa_damping', type=float, default=1.0,
                     help='Anderson acceleration damping')
 
 parser.add_argument('--plot_off', dest='makeplots', action='store_false',
@@ -352,53 +525,49 @@ parser.add_argument('--plot_iters', type=int, nargs=2, default=[0, 200],
 # parse command line args
 args = parser.parse_args()
 
-# sanity checks
-if (args.alpha < 1.0 or args.beta < 1.0) and args.aa_damping < 1.0:
-    print("=========================================================")
-    print("WARNING: Mixing internal relaxation and Anderson damping!")
-    print("=========================================================")
-
-print("Profile damping (alpha): ", args.alpha)
-print("Flux damping (beta):     ", args.beta)
-print("Acceleration depth:      ", args.aa_m)
-print("Acceleration delay:      ", args.aa_delay)
-print("Anderson damping:        ", args.aa_damping)
-
 # setup the problem
 Problem.setup(args)
 
 # initial profile
 profile = Problem.n_IC.copy()
 
-# initial flux
-flux = Problem.fluxModel.get_flux(profile)
+if args.gfun == 'pq':
+    # initial flux
+    flux = Problem.fluxModel.get_flux(profile)
 
-# create state vector
-state = np.concatenate((profile, flux))
+    # create state vector
+    state = np.concatenate((profile, flux))
+else:
+    state = profile.copy()
 
 # solve the problem
 Problem.solveKINSOL(state)
 
 # extract profile and flux
-profile = state[:Problem.N]
-flux = state[Problem.N:]
+if args.gfun == 'pq':
+    profile = state[:Problem.N]
+    flux = state[Problem.N:]
+else:
+    profile = state[:Problem.N]
+    flux = Problem.fluxModel.get_flux(profile)
 
 # shortcut to iteration count
 iter_idx = Problem.iter_idx
 
 # compute the final residual and error
 
-print("Profile damping (alpha): ", args.alpha)
-print("Flux damping (beta):     ", args.beta)
-print("Anderson damping:        ", args.aa_damping)
-print("Acceleration depth:      ", args.aa_m)
-print("Acceleration delay:      ", args.aa_delay)
-print("Iterations:              ", iter_idx)
-print("Residual:                ", Problem.residual_history[iter_idx - 1])
-print("Error:                   ", Problem.error_history[iter_idx - 1])
+print("Profile damping (alpha):                ", args.alpha)
+print("Flux damping (beta):                    ", args.beta)
+print("Transport coefficient damping (gamma):  ", args.gamma)
+print("Acceleration depth:                     ", args.aa_m)
+print("Acceleration delay:                     ", args.aa_delay)
+print("Anderson damping:                       ", args.aa_damping)
+print("Iterations:                             ", iter_idx)
+print("Residual:                               ", Problem.residual_history[iter_idx - 1])
+print("Error:                                  ", Problem.error_history[iter_idx - 1])
 
-# sanity checks (again)
-if (args.alpha < 1.0 or args.beta < 1.0) and args.aa_damping < 1.0:
+if ((args.alpha < 1.0 or args.beta < 1.0 or args.gamma < 1.0) and
+    args.aa_damping < 1.0):
     print("=========================================================")
     print("WARNING: Mixing internal relaxation and Anderson damping!")
     print("=========================================================")
@@ -466,22 +635,25 @@ if args.makeplots:
 # save the residual and error history
 residual_error_history = np.vstack((Problem.residual_history, Problem.error_history))
 
-outdir = "output_kinsol_pq"
+outdir = "output_kinsol_gfun" + args.gfun
 if args.noise:
     outdir += '_noise'
 outdir += '_p_' + str(args.p)
+outdir += '_N_' + str(args.N)
 
 if not os.path.exists(outdir):
     os.makedirs(outdir)
 
-prefix = 'kinsol_pq'
+prefix = 'kinsol_gfun' + args.gfun
 if args.noise:
     prefix += '_noise'
 prefix += '_p_' + str(args.p)
 prefix += '_alpha_' + str(args.alpha)
 prefix += '_beta_' + str(args.beta)
+prefix += '_gamma_' + str(args.gamma)
 prefix += '_m_' + str(args.aa_m)
 prefix += '_delay_' + str(args.aa_delay)
+prefix += '_damp_' + str(args.aa_damping)
 
 filename = prefix + '_residual_error_history.txt'
 
