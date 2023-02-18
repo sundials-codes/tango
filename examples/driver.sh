af80fca4010c8f85f6e5462c04d909a6190ae9df
David J. Gardner <gardner48@llnl.gov>
2023-02-18 13:07:23 -0800

    rename script

--- examples/driver.sh
+++ examples/driver.sh
@@ -1,38 +1,38 @@
 #!/bin/bash
 
-# python stripped_down_tango_example_kinsol_2.py \
+# python stripped_down_tango_example_kinsol_gptune.py \
 #        --beta 0.10
 
-# python stripped_down_tango_example_kinsol_2.py \
+# python stripped_down_tango_example_kinsol_gptune.py \
 #        --beta 0.20
 
-# python stripped_down_tango_example_kinsol_2.py \
+# python stripped_down_tango_example_kinsol_gptune.py \
 #        --beta 0.30
 
-# python stripped_down_tango_example_kinsol_2.py \
+# python stripped_down_tango_example_kinsol_gptune.py \
 #        --beta 0.40
 
-# python stripped_down_tango_example_kinsol_2.py \
+# python stripped_down_tango_example_kinsol_gptune.py \
 #        --beta 0.50
 
-# python stripped_down_tango_example_kinsol_2.py \
+# python stripped_down_tango_example_kinsol_gptune.py \
 #        --beta 0.60
 
 rm -rf output/
 
-python stripped_down_tango_example_kinsol_2.py \
+python stripped_down_tango_example_kinsol_gptune.py \
        --beta 0.4
 
-python stripped_down_tango_example_kinsol_2.py \
+python stripped_down_tango_example_kinsol_gptune.py \
        --beta 0.4 --mAA 3
 
-python stripped_down_tango_example_kinsol_2.py \
+python stripped_down_tango_example_kinsol_gptune.py \
        --beta_adapt --mAA 3
 
-python stripped_down_tango_example_kinsol_2.py \
+python stripped_down_tango_example_kinsol_gptune.py \
        --beta 0.4 --mAA 5 --adaptmAA
 
-python stripped_down_tango_example_kinsol_2.py \
+python stripped_down_tango_example_kinsol_gptune.py \
        --beta 0.4 --beta_adapt --mAA 5 --adaptmAA
 
 ./plot_output.py \
